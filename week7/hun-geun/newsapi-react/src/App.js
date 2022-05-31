72ba0fd63f16e2e7b63a795e2f977f7a50044d6f
Hungeun2 <bepoohun@gmail.com>
2022-05-31 15:23:50 +0900

    feat: App.js add Login Component

--- week7/hun-geun/newsapi-react/src/App.js
+++ week7/hun-geun/newsapi-react/src/App.js
@@ -5,19 +5,22 @@ import ResultNumber from './componenet/ResultNumber';
 import SearchBar from './componenet/SearchBar';
 import React from 'react';
 
+import Login from './componenet/Login';
+
 function App() {
   const [news, setNews] = React.useState([]);
   return (
     <>
+    <Login/>
     <div className='container'>
       <header>NEWS SEARCH</header>
-        <SearchBar setNews={setNews}/>
-        {news.length !== 0 ? 
-        <>
-          <Result news={news}/>
-          <ResultNumber num={news.length}/>
-        </> 
-        :<Result news={news}/>}
+      <SearchBar setNews={setNews}/>
+      {news.length !== 0 ? 
+      <>
+        <Result news={news}/>
+        <ResultNumber num={news.length}/>
+      </> 
+      :<Result news={news}/>}
     </div>
     </>
   );
