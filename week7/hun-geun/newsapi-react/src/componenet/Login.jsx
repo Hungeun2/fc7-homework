e06904b188923beb93f5dbb8e393a1412f59dc27
Hungeun2 <bepoohun@gmail.com>
2022-05-31 15:26:28 +0900

    feat: Add Login Component jsx file

---
+++ week7/hun-geun/newsapi-react/src/componenet/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import style from './Login.module.css'
+import { validEmail, validPassword } from './LoginValid'
+
+export default function Login() {
+    
+    const [id, setId] = useState("")
+    const [password, setPassword] = useState("")
+    const [idPlace, setIdPlace] = useState("")
+    const [passwordPlace, setPasswordPlace] = useState("")
+
+    const LoginFunc = (e) => {
+        e.preventDefault()
+        if(IsValidEmail() && IsValidPassword()){
+            alert("로그인 성공!")
+        } else if (!IsValidEmail()){
+            setId("")
+            setIdPlace("Check ID")
+        } else if (!IsValidPassword()){
+            setPassword("")
+            setPasswordPlace("Check Password")
+        } else {
+            setId("")
+            setPassword("")
+            setIdPlace("Check ID")
+            setPasswordPlace("Check Password")
+        }
+    }
+    const IsValidEmail = () => {
+        return ( validEmail.test(id)) ? true : false
+    }
+    const IsValidPassword = () => {
+        return ( validPassword.test(password)) ? true : false
+    }
+
+    return (
+        <form className={style.login} onSubmit={LoginFunc}>
+            <label htmlFor="id" >ID</label>
+            <input type="text" onChange={(e)=>{setId(e.target.value)}} value={id} placeholder={idPlace}/>
+            <br />
+            <label htmlFor="password">Password</label>
+            <input type="password" onChange={(e)=>{setPassword(e.target.value)}} value={password} placeholder={passwordPlace}/>
+            <br />
+            <button type="submit">로그인</button>
+            <br />
+        </form>
+    )
+}
+
